a0f6adc044e5d7188115f88be61ab14c6cf27786
vibakar <vibakar23@gmail.com>
2020-05-18 19:46:27 +0530

    Added loader and virtual scrolling

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-auth',
@@ -8,14 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-
-  constructor(private authService: AuthService, private router: Router) { }
+  isLoading: boolean = false;
+  constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
   }
 
   onClickLogin() {
+    this.isLoading = true;
     this.authService.login();
-    this.router.navigateByUrl('/places/tabs/discover');
+    this.loadingCtrl
+      .create({keyboardClose: true, message: 'Logging in...'})
+      .then(loadingEl => {
+        loadingEl.present();
+        setTimeout(() => {
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/places/tabs/discover');
+        }, 1500);
+      })
   }
 }
--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
 
@@ -9,10 +10,16 @@ import { Place } from '../place.model';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listLoadedPlaces: Place[];
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listLoadedPlaces = this.loadedPlaces.slice(1);
+  }
+
+  onFilterChange(event: CustomEvent) {
+    console.log(event.detail)
   }
 
 }
